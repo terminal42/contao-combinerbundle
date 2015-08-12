638a534a8e7606b1f4d083ce3180b04ea56b545c
Andreas Schempp <andreas.schempp@terminal42.ch>
2015-08-12 09:05:28 +0200

    Fixed checkbox options

--- src/Resources/contao/dca/tl_layout.php
+++ src/Resources/contao/dca/tl_layout.php
@@ -9,9 +9,10 @@ $GLOBALS['TL_DCA']['tl_layout']['palettes']['default'] .= ',combiners';
  * Fields
  */
 $GLOBALS['TL_DCA']['tl_layout']['fields']['combiners'] = [
-    'label'     => &$GLOBALS['TL_LANG']['tl_layout']['combiners'],
-    'exclude'   => true,
-    'inputType' => 'checkbox',
-    'eval'      => ['multiple' => true, 'tl_class' => 'clr'],
-    'sql'       => "blob NULL",
+    'label'      => &$GLOBALS['TL_LANG']['tl_layout']['combiners'],
+    'exclude'    => true,
+    'inputType'  => 'checkbox',
+    'foreignKey' => 'tl_combiner.name',
+    'eval'       => ['multiple' => true, 'tl_class' => 'clr'],
+    'sql'        => "blob NULL",
 ];
